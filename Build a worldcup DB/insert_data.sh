8861c76e2ea134a550f6ca0ba0ac77df4e27b07f
isaacbugeja <77076042+isaacbugeja@users.noreply.github.com>
2024-08-06 20:29:10 +0200

    Update insert_data.sh

--- Build a worldcup DB/insert_data.sh
+++ Build a worldcup DB/insert_data.sh
@@ -8,6 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
 #Clear tables
 echo $($PSQL "TRUNCATE TABLE teams, games RESTART IDENTITY")
 
@@ -46,6 +47,28 @@ do
       then
       echo Inserted into teams table: $OPPONENT
       fi
+    fi
+  fi
+
+done
+
+# Go through games.csv to input the corresponding data to games table
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+# Take year, round, winner goals, opponent goals and insert into corresponding columns
+  if [[ $YEAR != "year" ]]
+  then
+    # Get winner_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    GAME_ENTRY=$($PSQL "SELECT game_id FROM games WHERE year=$YEAR AND round='$ROUND' AND winner_id=$WINNER_ID AND opponent_id=$OPPONENT_ID AND winner_goals=$WINNER_GOALS AND opponent_goals=$OPPONENT_GOALS")
+
+    if [[ -z $GAME_ENTRY ]]
+    then
+      # If game_id does not exist, insert new entry
+      INSERT_GAME_IDS=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+      echo Inserted following into Games table: $YEAR, $ROUND, $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS. Total Count: $($PSQL "SELECT COUNT(game_id) FROM games")
     fi
   fi
 done
