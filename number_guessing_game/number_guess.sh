9dbd6d37c559b629a24f12ce1b3ce197a47847d1
isaacbugeja <77076042+isaacbugeja@users.noreply.github.com>
2024-08-17 17:17:58 +0200

    Update number_guess.sh

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -1,28 +1,66 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-
-echo -e "\nEnter your username: "
-read USERNAME
-SEARCH_USERNAME=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
-#if username does not exist in DB
-if [[ -z $SEARCH_USERNAME ]]
+random_number=$((1 + RANDOM % 1000))
+#Get username
+echo -e "\nEnter your username:"
+read username
+#Search DB for username
+check_user_exists=$($PSQL "SELECT user_id FROM users WHERE username='$username';")
+#If username does not exist
+if [[ -z $check_user_exists ]]
 then
-  #print Welcome, <username>! It looks like this is your first time here.
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-  #add user to DB
-  ADD_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
-  #give confirmation to user that registration was successful
-  if [[ $ADD_NEW_USER == "INSERT 0 1" ]]
+  #Create username and rest of prompt
+  insert_user_result=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$username', 0, 0);")
+  if [[ $insert_user_result == "INSERT 0 1" ]]
   then
-    echo -e "\nYou have been registered."
-  else
-    echo -e "\nRegistration has failed."
+    echo -e "\nWelcome, $username! It looks like this is your first time here."
   fi
-else
-  #else print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
-  echo -e "\nWelcome back, $USERNAME!" #to add rest of welcome statement
+else #if username does exist
+  get_games_played=$($PSQL "SELECT games_played FROM users WHERE user_id=$check_user_exists")
+  get_best_game=$($PSQL "SELECT best_game FROM users WHERE user_id=$check_user_exists")
+  echo -e "\nWelcome back, $username! You have played $get_games_played games, and your best game took $get_best_game guesses."
 fi
 
-#generate random number and store in variable
+#Give welcome back message and rest of prompt
+get_user_id=$($PSQL "SELECT user_id FROM users WHERE username='$username';") # Gets user ID after registration/Welcome back message.
+echo -e "\nGuess the secret number between 1 and 1000:"
+user_guess=0 # Initialize variable and set to 0
+number_of_guesses=0 # Initialize counter to 0
+while [[ $user_guess -ne $random_number ]]
+do
+  read user_guess
+  #Check if user input is a number
+  if [[ $user_guess =~ ^[0-9]+$ ]]
+  then
+    #Increment guess count by 1
+    number_of_guesses=$((number_of_guesses + 1))
+    
+    #Check if number is lower than random num
+    if [[ $user_guess -lt $random_number ]]
+    then
+      echo -e "\nIt's higher than that, guess again:"
+    elif [[ $user_guess -gt $random_number ]]
+    then
+      echo -e "\nIt's lower than that, guess again:"
+    fi
+  else
+    echo -e "\nThat is not an integer, guess again:"
+  fi
+done
 
-#play game to guess number
+echo -e "\nYou guessed it in $number_of_guesses tries. The secret number was $random_number. Nice job!"
+#Insert relevant data into DB
+#Get games played and increment by 1 and update DB
+get_games_played=$($PSQL "SELECT games_played FROM users WHERE user_id=$get_user_id")
+get_games_played=$((get_games_played + 1))
+update_games_played=$($PSQL "UPDATE users SET games_played = $get_games_played WHERE user_id=$get_user_id")
+
+#Get best_game info from DB
+get_best_game=$($PSQL "SELECT best_game FROM users WHERE user_id=$get_user_id")
+if [[ $get_best_game == "0" ]]
+then
+  update_best_game=$($PSQL "UPDATE users SET best_game = $number_of_guesses WHERE user_id = $get_user_id")
+elif [[ $get_best_game -gt $number_of_guesses ]]
+then
+  update_best_game=$($PSQL "UPDATE users SET best_game = $number_of_guesses WHERE user_id = $get_user_id")
+fi
